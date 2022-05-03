116d8e0e17f76d9d77d0cf2eb84b37b0496b6764
Dear_Jao <jvvr2013@gmail.com>
2022-05-03 12:56:06 -0300

    corrigido com eslint

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
+/* eslint-disable spaced-comment */
 const pixelBoard = document.querySelector('#pixel-board');
 const colorsButtons = document.getElementsByClassName('color');
 
-
-
-//mudas a cor selecionada
+//muda a cor selecionada
 function changeColors(event) {
-    const selected = document.querySelector('.selected')
-    selected.classList.remove('selected');
-    event.target.classList.add('selected');
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
 //torna as cores botões
-for (let btn of colorsButtons) {
-    btn.addEventListener('click', changeColors)
+// eslint-disable-next-line no-restricted-syntax
+for (const btn of colorsButtons) {
+  btn.addEventListener('click', changeColors);
 }
+
 //pinta os pixels
 const pixels = document.querySelectorAll('.pixel');
 
-for (pixelsCount = 0; pixelsCount < pixels.length; pixelsCount += 1) {
+for (let pixelsCount = 0; pixelsCount < pixels.length; pixelsCount += 1) {
+  // eslint-disable-next-line no-use-before-define
   pixels[pixelsCount].addEventListener('click', pixelSelected);
 }
 
+//escolha o pixels e pinta
 function pixelSelected(event) {
   const selectedColor = document.querySelector('.selected');
   const activeColor = window.getComputedStyle(selectedColor);
   const bgColor = activeColor.getPropertyValue('background-color');
+  // eslint-disable-next-line no-param-reassign
   event.target.style.backgroundColor = bgColor;
 }
 
 // limpa o quadro
-let clearBoard = document.querySelector('#clear-board');
-  clearBoard.addEventListener('click', function () {
-    const pixelsColors = document.querySelectorAll('.pixel');
-      for (let i = 0; i < pixelsColors.length; i +=1){
-        pixelsColors[i].style.backgroundColor = 'white';
-      }
-    }
-  );
+const clearBoard = document.querySelector('#clear-board');
+const newLocal = () => {
+  const pixelsColors = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixelsColors.length; i += 1) {
+    pixelsColors[i].style.backgroundColor = 'white';
+  }
+};
+clearBoard.addEventListener('click', newLocal);
 
-//cria pixel 
-function createPixels(pixels){
-    for  (let index = 0; index < pixels * pixels; index += 1) {
-        const newDiv = document.createElement('div');
-        newDiv.className = 'pixel';
-        newDiv.addEventListener('click', pixelSelected);
-        // newDiv.addEventListener('click', clear)
-        pixelBoard.appendChild(newDiv);
-    }
+//cria pixels
+// eslint-disable-next-line no-shadow
+function createPixels(pixels) {
+  for (let index = 0; index < pixels * pixels; index += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    newDiv.addEventListener('click', pixelSelected);
+    pixelBoard.appendChild(newDiv);
+  }
 }
-createPixels(5)
+createPixels(5);
